444117c6a98b14c0ed25bc72f05b2b288008f64b
Michael <91434795+usernameisavailablee@users.noreply.github.com>
2022-06-03 12:37:46 +0300

    12

--- lb#12.pl
+++ lb#12.pl
@@ -10,7 +10,7 @@ pr(X,I):- 0 is X mod I,!,fail.
 pr(X,I):- I1 is I+1, pr(X,I1).
 
 m_s_u(X,Y):-m_s_u(X,X,Y),!.
-m_s_u(X,N,N):-
+m_s_u(X,N,N):-s
     0 is X mod N,
     pr(N),!.
 m_s_u(X,N,Y):-
@@ -28,3 +28,28 @@ m(N,Cur,X,M):-
 m(N,Cur,X,M):-
     NewCur is Cur - 1,
     m(N,NewCur,X,M)
+%12
+nod(X,0,X):-!.
+nod(X,Y,Z):- C is X mod Y,
+    nod(Y,C,Z).
+
+mult(X,Y):-mult(X,Y,1).
+mult(0,Y,Y):-!.
+mult(X,Y,Z):-
+    N is X div 10,
+    K is X mod 10,
+    P is (Z*K),
+    mult(N,Y,P).
+
+maxdel(X,Y):-maxdel(X,X,Y),!.
+maxdel(X,Y,Y):-
+    0 is X mod Y,
+    not(pr(Y)),!.
+maxdel(X,Y,Z):-
+    Y1 is Y-1,
+    maxdel(X,Y1,Z).
+
+task_12(X,Y):-
+    mult(X,K),
+    maxdel(X,Z),
+    nod(K,Z,Y).
